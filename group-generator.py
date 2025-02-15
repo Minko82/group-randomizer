ea1f59f201c678dc1d73dfd37d0deda81bf4744d
Minko82 <obesecoconut@protonmail.com>
2025-02-15 11:36:31 -0700

    optimizing list

--- group-generator.py
+++ group-generator.py
@@ -2,9 +2,13 @@ import random
 
 def create_random_groups():
 
-    user_input = input('Enter a list of items, separated by a space:')
+    # user_input = input('Enter a list of items, separated by a space:')
+    # user_list= user_input.split()
 
-    user_list= user_input.split()
+    user_list = input('Enter a list of items, separated by a space:').split()
+
+    group_size = int(input('Enter the number of groups:'))
+
     print(user_list)
 
 create_random_groups()
